08e35219572c89c722be8b83e642ceeb2a887df5
lakalam <lakalam@debian-ans01.tema>
2023-02-25 16:10:32 +0300

    //

--- App/app.py
+++ App/app.py
@@ -1,12 +1,11 @@
-import telebot;
-bot = telebot.TeleBot('6100720154:AAFQtSQgy85DpX4o0U88G5LYxTrrHReEbTo');
+import telebot
+bot=telebot.TeleBot('6100720154:AAFQtSQgy85DpX4o0U88G5LYxTrrHReEbTo')
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
- if message.text == "Привет":
-    bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
- elif message.text == "/help":
-    bot.send_message(message.from_user.id, "PIPSKA")
+ if message.text=="Привет":
+    bot.send_message(message.from_user.id,"Привет, чем я могу тебе помочь?")
+ elif message.text=="/help":
+    bot.send_message(message.from_user.id,"PIPSKA")
  else:
-    bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши PIPSKA!!!!.")
-    
-#bot.polling(none_stop=True, interval=5)    
+    bot.send_message(message.from_user.id,"Я тебя не понимаю. Напиши PIPSKA!!!!.")
+ #bot.polling(none_stop=True, interval=5) 
